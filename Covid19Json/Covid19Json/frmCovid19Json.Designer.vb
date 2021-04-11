5a62d7457b3284e97172afa694c681898b6bc92d
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-04-11 14:34:17 +0700

    Add files via upload

---
+++ Covid19Json/Covid19Json/frmCovid19Json.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCovid19Json
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbCountry = New System.Windows.Forms.ComboBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgvData
+        '
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvData.Location = New System.Drawing.Point(3, 19)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.RowTemplate.Height = 27
+        Me.dgvData.Size = New System.Drawing.Size(1134, 677)
+        Me.dgvData.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.dgvData)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 42)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1140, 699)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = " Coronavirus API "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select"
+        '
+        'cmbCountry
+        '
+        Me.cmbCountry.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbCountry.FormattingEnabled = True
+        Me.cmbCountry.Location = New System.Drawing.Point(63, 12)
+        Me.cmbCountry.Name = "cmbCountry"
+        Me.cmbCountry.Size = New System.Drawing.Size(181, 24)
+        Me.cmbCountry.TabIndex = 1
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(352, 12)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(161, 23)
+        Me.txtSearch.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(250, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Search Country"
+        '
+        'frmCovid19Json
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1145, 742)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.cmbCountry)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmCovid19Json"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "[VB.NET] Covid-19 with JSON - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dgvData As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmbCountry As System.Windows.Forms.ComboBox
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Covid19Json/Covid19Json/frmCovid19Json.vb
@@ -0,0 +1,249 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: Coronavirus API for Current cases by country COVID-19.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+#End Region
+
+Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Converters
+
+Public Class frmCovid19Json
+
+    Dim table As New DataTable
+
+    Private Sub frmCovid19Json_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Call GetCountry()
+    End Sub
+
+    Private Sub cmbCountry_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cmbCountry.SelectedIndexChanged
+        Dim url As String = String.Empty
+        Select Case cmbCountry.Text
+            Case "- All Countries Info -"
+                url = "https://coronavirus-19-api.herokuapp.com/countries"
+            Case Else
+                url = "https://coronavirus-19-api.herokuapp.com/countries/" & cmbCountry.Text
+        End Select
+        '//
+        Call GetDataJson(url)
+        'Call GetDataJsonObject(url)
+    End Sub
+
+    '// Deserialized JSON and return DataTable.
+    Public Function DeserializeDataTable(json As String) As DataTable
+        Dim dt As DataTable = TryCast(JsonConvert.DeserializeObject(json, (GetType(DataTable))), DataTable)
+        Return dt
+    End Function
+
+    Private Sub GetCountry()
+        Dim request As HttpWebRequest
+        Dim response As HttpWebResponse = Nothing
+        Dim reader As StreamReader
+        Dim url As String = "https://coronavirus-19-api.herokuapp.com/countries"
+        Try
+            request = DirectCast(WebRequest.Create(url), HttpWebRequest)
+            response = DirectCast(request.GetResponse(), HttpWebResponse)
+            reader = New StreamReader(response.GetResponseStream())
+            '//
+            Dim s As String
+            s = reader.ReadToEnd
+            '// Replace null value from JSON to zero.
+            s = s.Replace("null", "0")
+            Dim dt As DataTable
+            dt = DeserializeDataTable(s)
+            With cmbCountry
+                .Items.Add("- All Countries Info -")
+            End With
+            table.Columns.Add("Country", GetType(String))
+            '// Add all country into ComboBox.
+            For i = 0 To dt.Rows.Count - 1
+                cmbCountry.Items.Add(dt.Rows(i).Item(0).ToString)
+                '// AutoComplete.
+                table.Rows.Add(dt.Rows(i).Item(0).ToString)
+            Next
+            cmbCountry.SelectedIndex = 0
+            cmbCountry.Sorted = True
+
+            '// AutoComplete in TextBox Control.
+            Dim DataCollection As New AutoCompleteStringCollection()
+            DataCollection = GetAutoSourceCollection(table)
+            txtSearch.AutoCompleteCustomSource = DataCollection
+            txtSearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend
+            txtSearch.AutoCompleteSource = AutoCompleteSource.CustomSource
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '// AutoComplete Collection
+    Private Function GetAutoSourceCollection(ByVal table As DataTable) As AutoCompleteStringCollection
+        Dim AutoSourceCollection As AutoCompleteStringCollection = New AutoCompleteStringCollection()
+        '// table from countries.
+        For Each row As DataRow In table.Rows
+            AutoSourceCollection.Add(row(0).ToString) '// Country data is the first column.
+        Next
+        Return AutoSourceCollection
+    End Function
+
+    '// Get JSON From URL.
+    Private Sub GetDataJson(ByVal url As String)
+        Dim request As HttpWebRequest
+        Dim response As HttpWebResponse = Nothing
+        Dim reader As StreamReader
+        Try
+            request = DirectCast(WebRequest.Create(url), HttpWebRequest)
+            response = DirectCast(request.GetResponse(), HttpWebResponse)
+            reader = New StreamReader(response.GetResponseStream())
+            Dim s As String
+            s = reader.ReadToEnd
+            s = s.Replace("null", "0")
+            Dim dt As DataTable
+            If Microsoft.VisualBasic.Left(s, 1) = "[" Then
+                dt = DeserializeDataTable(s)
+            Else
+                dt = DeserializeDataTable("[" & s & "]")
+            End If
+            dgvData.DataSource = dt
+            Call SetupGridView()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '// Get JSON by Object From URL.
+    Private Sub GetDataJsonObject(ByVal url As String)
+        Dim request As HttpWebRequest
+        Dim response As HttpWebResponse = Nothing
+        Dim reader As StreamReader
+        Try
+            request = DirectCast(WebRequest.Create(url), HttpWebRequest)
+            response = DirectCast(request.GetResponse(), HttpWebResponse)
+            reader = New StreamReader(response.GetResponseStream())
+            Dim s As String
+            s = reader.ReadToEnd.Replace("null", "0")
+            If Microsoft.VisualBasic.Left(s, 1) <> "[" Then
+                s = "[" & s & "]"
+            End If
+            '//
+            Dim res() = Newtonsoft.Json.JsonConvert.DeserializeObject(Of ItemsCovid())(s)
+            Dim dt As New DataTable
+            dt.Columns.Add("country", GetType(String))
+            dt.Columns.Add("cases")
+            dt.Columns.Add("todayCases")
+            dt.Columns.Add("deaths")
+            dt.Columns.Add("todayDeaths")
+            dt.Columns.Add("recovered")
+            dt.Columns.Add("active")
+            dt.Columns.Add("critical")
+            dt.Columns.Add("casesPerOneMillion", GetType(Integer))
+            For Each covid As ItemsCovid In res
+                Dim dr As DataRow = dt.NewRow()
+                If covid IsNot Nothing Then
+                    dr(0) = covid.country
+                    dr(1) = covid.cases
+                    dr(2) = covid.todayCases
+                    dr(3) = covid.deaths
+                    dr(4) = covid.todayDeaths
+                    dr(5) = covid.recovered
+                    dr(6) = covid.active
+                    dr(7) = covid.critical
+                    dr(8) = covid.casesPerOneMillion
+                End If
+                '// Add row.
+                dt.Rows.Add(dr)
+            Next
+            dgvData.DataSource = dt
+            Call SetupGridView()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub txtSearch_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles txtSearch.KeyDown
+        '// Press Enter
+        If e.KeyCode = Keys.Enter Then
+            e.Handled = True
+            '// Search for Country in DataTable.
+            Dim dv As DataView = New DataView(table)
+            dv.RowFilter = "Country = " & "'" & txtSearch.Text & "'"
+            If dv.Count > 0 Then
+                Dim url As String = "https://coronavirus-19-api.herokuapp.com/countries/" & txtSearch.Text
+                '// Second Methods
+                Call GetDataJsonObject(url)
+            End If
+        End If
+    End Sub
+
+    Private Sub SetupGridView()
+        With dgvData
+            .RowHeadersVisible = True
+            .AllowUserToAddRows = False
+            .AllowUserToDeleteRows = False
+            .AllowUserToResizeRows = False
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.CellSelect
+            .ReadOnly = True
+            '// Data rows
+            .Font = New Font("Tahoma", 9)
+            .RowTemplate.MinimumHeight = 27
+            .RowTemplate.Height = 27
+            '// Column Header
+            .ColumnHeadersHeight = 30
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            '// Autosize Column
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            '// Header
+            With .ColumnHeadersDefaultCellStyle
+                .BackColor = Color.Navy
+                .ForeColor = Color.White
+                .Font = New Font(dgvData.Font, FontStyle.Bold)
+            End With
+        End With
+        '/ todayDeaths
+        For i = 0 To dgvData.RowCount - 1
+            If dgvData.Rows(i).Cells(4).Value > 0 Then
+                dgvData.Rows(i).Cells(4).Style.BackColor = Color.Red
+            End If
+        Next
+        For i = 1 To dgvData.ColumnCount - 1
+            dgvData.Columns(i).DefaultCellStyle.Format = "N0"
+            With dgvData.Columns(i)
+                .HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            End With
+        Next
+    End Sub
+
+    Private Sub frmCovid19Json_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+End Class
+
+'// "country":"Thailand","cases":599,"todayCases":188,"deaths":1,"todayDeaths":0,"recovered":44,"active":554,"critical":7,"casesPerOneMillion":9
+Public Class ItemsCovid
+    Public Property country As String
+    Public Property cases As Integer
+    Public Property todayCases As Integer
+    Public Property deaths As Integer
+    Public Property todayDeaths As Integer
+    Public Property recovered As Integer
+    Public Property active As Integer
+    Public Property critical As Integer
+    Public Property casesPerOneMillion As Integer
+End Class
